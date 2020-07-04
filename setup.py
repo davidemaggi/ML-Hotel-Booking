6465c99d3f32ecd4daba7df9e86f44734e1ce524
Davide Maggi <davide.maggi@live.com>
2020-07-04 15:22:36 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ML-Hotel-Booking',
+    version='0.1',
+    packages=['towelstuff',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
